6cd08d42b01b10dc703c621207da7227be2e6f59
Marc Serret <vesperon51@gmail.com>
2021-12-28 00:42:12 +0100

    Added more items

--- pools/Pools.lua
+++ pools/Pools.lua
@@ -1,14 +1,14 @@
 local M = {}
 M.RED_POOL = {
+    105, -- The D6
+    97, --The book of sin
     554, --2Spooky
     647, -- 4.5 volts
     230, --abbadon
     706, --abyss
     641, --akeldama
-    65, --anarchist coockbook
     214, --anemic
     511, --angry fly
-    130, --a pony
     443, --apple
     672, --a pound of flesh
     699, --azazel rage
@@ -24,7 +24,6 @@ M.RED_POOL = {
     565, --blood puppy
     157, --bloody lust
     509, --bloodshot eye
-    186, --blood rights
     695, --bloody gust
     607, --boiled baby
     629, --bot fly
@@ -128,6 +127,8 @@ M.RED_POOL = {
     402, --chaos
 }
 M.PINK_PURPLE_POOL = {
+    158, --crystal ball
+    65, --anarchist coockbook
     191, --3 dollar bill
     493, --adrenaline
     369, --continuum
@@ -145,8 +146,24 @@ M.PINK_PURPLE_POOL = {
     681, --lil portal
     439, --moms box
     312, --maggys bow
+    426, --obsessed fan
+    581, --psy fly
+    141, --pageant boy
+    389, --rune bag
+    218, --placenta
+    563, --nancy bombs
+    139, --moms purse
+    508, --moms razor
+    197, --jesus juice
+    149, --IPECAC
+    242, --infamy
+    566, --dream catcher
 }
 M.ORANGE_YELLOW_POOL = {
+    102, -- Mom bottle of pills
+    192, --telepathy for dummies
+    282, --How to jump
+    586, --Starway
     491, --acid baby
     228, --moms perfume
     582, --rocket in a jar
@@ -162,8 +179,34 @@ M.ORANGE_YELLOW_POOL = {
     625, --mega mush
     201, -- mom bottle of pills
     555, -- golden razor
+    501, -- explosivo
+    499, --eucharist
+    603, --battery pack
+    199, --moms key
+    295, --magic fingers
+    240, -- experimental treatment
+    602, --member card
+    6, --number one
+    517, --fasts bombs
+    568, --divine intervention
+    238, --key piece 1
+    239, --key piece 2
+    502, --large zit
+    429, --head if keeper
+    414, --more options
+    75, --phd
+    578, --free lemonade
+    56, --lemon mishap
+    146, --prayer card
+    294, --butter bean
+    484, --wait what
+    579, --spirit sword
+    387, --censer
+    404, --farting baby
 }
 M.GRAY_WHITE_POOL = {
+    34, -- book of belial
+    171, --spider butt
     245, --20/20
     359, --8 inch nails
     683, --bone spurs
@@ -188,19 +231,146 @@ M.GRAY_WHITE_POOL = {
     3, --spoon bender
     708,--stapler
     562, --rock bottom
+    400, --spear of destiny
+    17, --skeleton key
+    542, --slipped rib
+    205, --sharp plug
+    255, --screw
+    325, --scissors
+    366, --scatter bombs
+    321, --samson chains
+    339, --safety pin
+    126, --razor blade
+    661, --quints
+    190, --pyro
+    37, --mr. bomb
+    407, --purity
+    348, --placebo
+    380, --pay to play
+    670, --options?
+    219, --old bandage
+    378, --NO 2
+    433, --my shadow
+    631, --meat claver
+    41, --moms pad
+    431, --multidimensional baby
+    106, --mr mega
+    449, --metal plate
+    355, --moms pearls
+    436, --milk!
+    612, --lost soul
+    388, --key bum
+    194, --magic 8 ball
+    100, --little steven
+    99, --little gish
+    617, --lodestone
+    503, --litle horn
+    270, --leech
+    627, --knife piece 2
+    277, --lil haunt
+    275, --lil brimstone
+    712, --lemegeton
+    266, --juicy sack
+    413, --immaculate conception
+    293, --head of krampus
+    352, --glass canon
+    156, --habit
+    134, --guppy tail
+    145, --guppy head
+    225, --gimpy
+    727, --ghost bombs
+    495, --ghost pepper
+    163, --ghost baby
+    364, --friend zone
+    600, --eye drops
+    291, --flush!
+    450, --eye of greed
+    496, --euthanasia
+    609, --eternal d6
+    445, --dog tooth
+    381, --edens blessing
+    498, --duality
+    265, --dry baby
+    47, --doctors remote
+    547, --divorce papers
+    81, --dead cat
+    117, --dead bird
+    57, --distant admiration
+    259, --dark matter
+    442, --dark princess crown
+    170, --daddy long legs
+    278, --dark bum
+    283, --D100
+    455, --dads lost coin
+    406, --D8
+    437, --D7
+    476, --D1
+    316, --cursed eye
+    482, --clicker
+    160, --crack the sky
+    216, --ceremonial robes
+    263, --clear rune
+    62, --charm of the vampire
+    162, --celtic cross
+    423, --circle of protection
+    165, --CAT-O-NINE-TAILS
+    340, --caffeine pill
+    377, --bursting sack
+    129, --bucket of lard
+    549, --brittle bones
+    25, --breakfast
+    288, --box of spiders
+    19, --boom!
+    545, -- book of the dead
+    287, --book of secrets
+    125, --bobby-bomb
+    353, --bomber-boy
+    286, --blank card
+    420, --black powder
+    610, --bird cage
+    260, --black candle
+    274, --best bud
+    231, --ball of tar
+    408, --athame
+    132, --a lump of coal
+    161, --ankh
+    465, --analog stick
+    564, --a bar of soap
+    116, --nine volt
+    89, --spider byte
+    189, --SMB super fan
+    403, --spider mod
+    244, --tech.5
+    95, --robo-baby
+    267, --robo-baby 2.0
+    86, --monstro tooth
+    55, --moms eye
+    510, --delirious
+    330, --soy milk
+    58, --book of shadows
+    519, --lil delirium
+    585, --alabaster box
+    127, --forget me now
+    319, --cain other eye
+    675, --cracked orb
+    94, --sack of pennies
+    130, --a pony
 
-}
 M.BROWN_FLESH_POOL = {
+    33, -- The bible
+    111, --the bean
+    38, --tammy head
+    186, --blood rights
+    60, --ladder
     354, --crackjacks
     427, --minecrafter
-    510, --delirious
     173, --mitre
     234, --infestation 2
     577, --damocles
     688, --inner child
     729, --decap attack
     85, --deck of cards
-    125, --dead sea scrolls
+    124, --dead sea scrolls
     574, --monstrance
     115, --ouija board
     123, --monster manuel
@@ -255,7 +425,6 @@ M.BROWN_FLESH_POOL = {
     241, --contract from below
     457, --cone head
     69, --chocolate milk
-    154, --chemical peel
     480, --compost
     209, --butt bombs
     144, --bum friend
@@ -274,7 +443,7 @@ M.BROWN_FLESH_POOL = {
     217, --moms wig
     660, --card reading
     334, --match book
-    401, --explosivo
+    257, --fire mind
     29, --moms underwear
     252, --little baggy
     685, --jar of wisps
@@ -294,7 +463,6 @@ M.BROWN_FLESH_POOL = {
     120, --odd mushrom slim
     147, --notched axe
     487, --potato peeler
-    171, --spider butt
     175, --dads key
     16, --raw liver
     523, --moving box
@@ -314,6 +482,10 @@ M.BROWN_FLESH_POOL = {
     227, --piggy bank
 }
 M.BLUE_POOL = {
+    164, --The candle
+    338, --The boomerang
+    44, --teleport
+    323, --Isaac tears
     651, --bethelen
     516, --sprinkler
     588, --sol
@@ -335,5 +507,95 @@ M.BLUE_POOL = {
     428, --pjs
     593, --mars
     318, --gemini
+    313, -- holy mantle
+    430, --papa fly
+    297, --pandoras box
+    571, --orphan socks
+    461, --parasitoid
+    309, --pisces
+    598, --pluto
+    468, --shade
+    696, --salvation
+    71, --mini mush
+    200, -- moms eyeshadow
+    595, --saturnus
+    305, --scorpio
+    628, --death certificate
+    390, --seraphim
+    382, --frienly ball
+    720, --everything jar
+    652, --cube baby
+    301, --cancer
+    415, --crown of light
+    232, --stop watch
+    337, --broken watch
+    222, --antigravity
+    326, --breath of life
+    342, --blue cap
+    308, --aquarius
+    589, --luna
+    300, --aries
+    320, --??? only friend
+    559, --120 volt
+    561, --almond milk
+    535, --blanket
+    438, --binky
+    246, --blue map
+    584, --book of virtues
+    307, --capricorn
+    8, --brother bobby
+    386, --D12
+    296, --converter
+    371, --curse of the tower
+    622, --genesis
+    410, --evil eye
+    632, --evil charm
+    361, --fates reward
+    608, --freezer baby
+    490, --eden soul
+    179, --fate
+    368, --epiphoria
+    540, --flat stone
+    128, --forever alone :sad:
+    422, --glowing hourglass
+    460, --glaucoma
+    374, --holy shot
+    464, --gliph of balance
+    543, --hallowed sanctuary
+    594, --jupiter
+    470, --hushy
+    494, --jacob ladder
+    717, --keeper kin
+    532, --lachryphagy
+    472, -- king baby
+    248, --hive mind
+    379, --pupula duplex
+    304, --libra
+    302, --leo
+}
+M.GREEN_POOL = {
+    11, -- 1 up!
+    18, --a dollar
+    284, -- D4
+    690, --Belly jelly
+    273, --bobs brain
+    521, --coupon
+    140, --bobs course
+    268, --rotten baby
+    459, --sinus infection
+    14, --roid rage
+    393, --serpents kiss
+    42, --bob rotten head
+    317, --misterious liquid
+    505, --poke go
+    153, --mutant spider
+    478, --pause
+    210, --gnawed leaf
+    350, --toxic shock
+    1, --Sad onion
+    639, --Yuck heart
+    154, --chemical peel
+    103, --the commom cold
+
 }
 return M
